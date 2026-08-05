560fa3d6e5325dfc35834cefc8152c8a4ce8ace0
ushanovviachaslaugit <ushanov.viachaslau.git@gmail.com>
2026-08-05 11:29:11 +0300

    Первый коммит: Модуль 1, задания 1-4

---
+++ about_me.py
@@ -0,0 +1,15 @@
+#Создание переменных данных
+my_name = "Slava"
+my_age = 30
+my_city = "Gomel"
+hobbies = ["Sport", "movi's"]
+#Вывод информации
+print("Меня зовут", my_name)
+print("Мой возраст", my_age)
+print("Я проживаю", my_city)
+print("Мои увлечения", hobbies[0])
+#Первое использование моманды _input()
+user_input = input("А как тебя зовут")
+print("Приятно познакомится, ",user_input)
+user_input = input("Какое у тебя хобби?")
+print("Мое хобби:",hobbies[1])
